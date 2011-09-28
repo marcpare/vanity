c91c7c994115317d86be164033472f32802801b8
unknown <marc@.(none)>
2011-09-28 23:11:48 +0700

    php revamp

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include("header.php");
+?>
+
+<p>
+<img src="img/header.jpg" width="600px" />
+</p>
+
+<p>
+I am part of a growing community of design engineers that see technology's future as more than just bigger and faster for the developed world. You'll find us spread around the world, chasing opportunities to reshape the digital and physical landscape into something simpler and more human. Some of us are motivated by frustration with the piles of meaningless stuff around us. Others by the uncertain future of our planet. This pursuit is necessarily international and interdisciplinary; as a result, our travel diaries and research agendas look scattered even though we're not wandering.
+</p>
+
+<p>
+Currently, I am a clean energy entrepreneur working on a 1,000 year old problem in Southeast Asian traditional brick-making. In the developing world, there is no such thing as purely a "technology problem", which means that I employ the tools of ethnography, social enterprise, and IT to understand and affect the sociotechnical processes that result in a red clay brick. I am fascinated by the potential of market forces to solve social and environmental problems as well as the possibilities of appropriate technology. 
+</p>
+
+<hr />
+
+<p>You can find plenty about me on the rest of the pages of this site or on Google. But here is a snapshot of my past experience if you don't have much time:</p>
+
+<ul>
+<li>B.S. Mechanical Engineering, Georgia Institute of Technology</li>
+<li>3rd overall Senior Design project for novel improved cook stove design</li>
+<li>Microsoft Imagine Cup World Champion, 2009</li>
+<li>International recognition for competitive algorithm programming  [ACM ICPC World Finalist, 2008]</li>
+<li> 
+    <p>Internships:</p>
+    <ul>
+    <li>NASA Jet Propulsion Lab</li>
+    <li>AREVA Nuclear Power</li>
+    <li>Shaw Industries (Berkshire Hathaway, Inc.)</li>
+    </ul>
+</li>
+<li>
+    <p>Research:</p>
+    <ul>
+    <li>Finite Element Modeling of ultrasonic wave propagation through solid media</li>
+    <li>Mathematical Programming for system-level optimization of hydraulic hybrid vehicle</li>
+    </ul>
+</li>
+
+<li>Foreign Languages: French</li>
+</ul>
+
+<p>
+Some of the most interesting things about me online include: the <a href="letters.html">letter I wrote before leaving for Vietnam</a>, the tumblr documenting our <a href="http://smallredtile.tumblr.com">cook stove design process</a>, a <a href="http://www.gatech.edu/president/lettersbulletins/july2009.html">mention by the President of Georgia Tech</a>, and <a href="http://www.slideshare.net/sirgawain33/state-of-the-art-in-rice-husk-gasification">my talk at the ETHOS conference</a> about rice husk gasification.
+</p>
+
+
+<?php
+include 'footer.php';
+?>
---
+++ letters.php
@@ -0,0 +1,190 @@
+<?php
+include 'header.php'
+?>
+
+<h1>/letters</h1>
+
+<p>Leaving for Vietnam, February 2009</p>
+
+<p>So, I was working on improved cook stoves all last semester for rural Nicaragua (see: [1] ) Along the way I met people from countries all over the world. One of those guys was a retired recycling engineer living in Da Lat, Vietnam, who was building exactly the kind of stove I was working with (rice husk gasification) in order to get rid of waste rice husk / make cooking safer. He invited me to come help him a few months ago. I didn't know what to do at first, but as the semester went on, I couldn't stop thinking about it. The prospect of a new language, design for the developing world, microeconomic solutions to real problems, entrepreneurial training: it was very alluring.</p>
+
+<p>There's this interesting computational aspect to it all, too; the challenge to this sort of work is often in finding and organizing design information as well as managing online collaboration from many countries/cultures. There are opportunities for collecting good data and doing good science. Policy-makers are itching for statistically significant results to help guide their efforts in coming years.</p>
+
+<p>I decided to help Paul with his work in Vietnam.</p>
+
+<p>I found out that this work with appropriate technology is infectious. Our senior design group tested cook stoves outside at Georgia Tech for an entire semester. People walking by loved talking about fire and would poke their heads over the reactor (even if it threatened singed eyebrows). We taught Engineers Without Borders how to make <em>tincanium</em> stoves, and they brought the designs to Cameroon. This semester, they've already had dozens of undergraduate engineers volunteer to tinker with their own stoves. There are many groups like this around the world, but they are not yet connected very well. It is going to be exciting when online community managers and Open Source ethos make their way into the group. (hint: we're already scheming)</p>
+
+<p>Something important to me is that this be a real, humble effort (in contrast to an overblown, imaginary one). For better or for worse, university curriculums stick to the abstract. Interpreted the wrong way, this leads to students that skim by with ideas that are never tested against reality. I was no exception (example: thinking that because a problem was complicated made it interesting). Muhammad Yunus said the same about his time teaching in the States, calling it a "caricature". There are people at Georgia Tech trying to change that, and I really hope that they succeed. And, of course, there are people there already doing it right. I do still get excited by reading Buckminster Fuller, thinking that I can be an "anticipatory design scientist" and affect the course of civilization through technology. What I am really excited about, though, is getting to add more cuts to my hands in the quest to fifty and finally making it as a real stove designer.</p> 
+
+<p>I am not sure where this will lead. Ostensibly, this trip is to make use of my time before entering graduate school. Paul, though, says that I won't be able to bring myself to leave Vietnam. The graduate programs I applied for will be a good intellectual follow-up to the issues that the work will bring up, but opportunities may arise between now and then that glimmer with even more of the magic that has drawn me to Vietnam.</p>
+
+<p>[1] <a href="http://notwandering.com/smallredtile.html">http://notwandering.com/smallredtile.html</a></p>
+
+<p>P.S. A big thanks to <a href="http://www.vouchboard.com">VouchBoard</a>, a small (for now) social startup that traded me the money for the trip for a PHP prototype of their idea.</p>
+
+<hr />
+
+<p>If you're interested, I'll send out a once monthly letter about the experience.</p>
+
+<p>I'll try to make it something Claude L&eacute;vi-Strauss would be happy to receive (i.e. not "le menu tant de d&eacute;tails inspides, d'&eacute;v&eacute;nements inginfiants"..."a blow-by-blow full of insipid details and insignificant events"). If you have anything you want me looking for while I'm out there, let me know.</p>
+
+<!-- Begin MailChimp Signup Form -->
+<script type="text/javascript">
+// delete this script tag and use a "div.mce_inline_error{ XXX !important}" selector
+// or fill this in and it will be inlined when errors are generated
+var mc_custom_error_style = '';
+</script>
+<div id="mc_embed_signup">
+<form action="http://notwandering.us2.list-manage2.com/subscribe/post?u=fd8f6bcee83029f0f8947b9c8&amp;id=dba3b4cc6f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+	<fieldset>
+	
+<div class="mc-field-group">
+<label for="mce-EMAIL">Email Address </label>
+<input type="text" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+		<div id="mce-responses">
+			<div class="response" id="mce-error-response" style="display:none"></div>
+			<div class="response" id="mce-success-response" style="display:none"></div>
+		</div>
+		<div><input type="submit" value="join" name="subscribe" id="mc-embedded-subscribe" class="btn"></div>
+	</fieldset>	
+</form>
+</div>
+<script  type="text/javascript">
+try {
+    var jqueryLoaded=jQuery;
+    jqueryLoaded=true;
+} catch(err) {
+    var jqueryLoaded=false;
+}
+if (!jqueryLoaded) {
+    var head= document.getElementsByTagName('head')[0];
+    var script= document.createElement('script');
+    script.type= 'text/javascript';
+    script.src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+    head.appendChild(script);    
+}
+</script>
+<script type="text/javascript" src="http://downloads.mailchimp.com/js/jquery.form-n-validate.js"></script>
+
+<script type="text/javascript">
+var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';var err_style = '';
+try{
+    err_style = mc_custom_error_style;
+} catch(e){
+    err_style = 'margin: 1em 0 0 0; padding: 1em 0.5em 0.5em 0.5em; background: ERROR_BGCOLOR none repeat scroll 0% 0%; font-weight: bold; float: left; z-index: 1; width: 80%; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial; color: ERROR_COLOR;';
+}
+var head= document.getElementsByTagName('head')[0];
+var style= document.createElement('style');
+style.type= 'text/css';
+if (style.styleSheet) {
+  style.styleSheet.cssText = '.mce_inline_error {' + err_style + '}';
+} else {
+  style.appendChild(document.createTextNode('.mce_inline_error {' + err_style + '}'));
+}
+head.appendChild(style);
+$(document).ready( function($) {
+  var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
+  var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
+  options = { url: 'http://notwandering.us2.list-manage.com/subscribe/post-json?u=fd8f6bcee83029f0f8947b9c8&id=dba3b4cc6f&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+                beforeSubmit: function(){
+                    $('#mce_tmp_error_msg').remove();
+                    $('.datefield','#mc_embed_signup').each(
+                        function(){
+                            var txt = 'filled';
+                            var fields = new Array();
+                            var i = 0;
+                            $(':text', this).each(
+                                function(){
+                                    fields[i] = this;
+                                    i++;
+                                });
+                            $(':hidden', this).each(
+                                function(){
+                                	if ( fields[0].value=='MM' && fields[1].value=='DD' && fields[2].value=='YYYY' ){
+                                		this.value = '';
+									} else if ( fields[0].value=='' && fields[1].value=='' && fields[2].value=='' ){
+                                		this.value = '';
+									} else {
+	                                    this.value = fields[0].value+'/'+fields[1].value+'/'+fields[2].value;
+	                                }
+                                });
+                        });
+                    return mce_validator.form();
+                }, 
+                success: mce_success_cb
+            };
+  $('#mc-embedded-subscribe-form').ajaxForm(options);
+
+});
+function mce_success_cb(resp){
+    $('#mce-success-response').hide();
+    $('#mce-error-response').hide();
+    if (resp.result=="success"){
+        $('#mce-'+resp.result+'-response').show();
+        $('#mce-'+resp.result+'-response').html(resp.msg);
+        $('#mc-embedded-subscribe-form').each(function(){
+            this.reset();
+    	});
+    } else {
+        var index = -1;
+        var msg;
+        try {
+            var parts = resp.msg.split(' - ',2);
+            if (parts[1]==undefined){
+                msg = resp.msg;
+            } else {
+                i = parseInt(parts[0]);
+                if (i.toString() == parts[0]){
+                    index = parts[0];
+                    msg = parts[1];
+                } else {
+                    index = -1;
+                    msg = resp.msg;
+                }
+            }
+        } catch(e){
+            index = -1;
+            msg = resp.msg;
+        }
+        try{
+            if (index== -1){
+                $('#mce-'+resp.result+'-response').show();
+                $('#mce-'+resp.result+'-response').html(msg);            
+            } else {
+                err_id = 'mce_tmp_error_msg';
+                html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
+                
+                var input_id = '#mc_embed_signup';
+                var f = $(input_id);
+                if (ftypes[index]=='address'){
+                    input_id = '#mce-'+fnames[index]+'-addr1';
+                    f = $(input_id).parent().parent().get(0);
+                } else if (ftypes[index]=='date'){
+                    input_id = '#mce-'+fnames[index]+'-month';
+                    f = $(input_id).parent().parent().get(0);
+                } else {
+                    input_id = '#mce-'+fnames[index];
+                    f = $().parent(input_id).get(0);
+                }
+                if (f){
+                    $(f).append(html);
+                    $(input_id).focus();
+                } else {
+                    $('#mce-'+resp.result+'-response').show();
+                    $('#mce-'+resp.result+'-response').html(msg);
+                }
+            }
+        } catch(e){
+            $('#mce-'+resp.result+'-response').show();
+            $('#mce-'+resp.result+'-response').html(msg);
+        }
+    }
+}
+</script>
+<!--End mc_embed_signup-->
+
+
+<?php
+include 'footer.php'
+?>
